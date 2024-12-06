864682c90be32908d095e94c4d49eb73a6a84d9c
luuk-badeend <lbattjes25@gmail.com>
2024-12-06 13:54:54 +0100

    renamed all to django_polls_app

--- django_polls_app/urls.py
+++ django_polls_app/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for weather_etl_project project.
+URL configuration for django_polls_app project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
