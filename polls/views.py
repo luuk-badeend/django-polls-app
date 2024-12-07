4bbe3bf8c501ab02db4d8d07eef9797574254bc8
luuk-badeend <lbattjes25@gmail.com>
2024-12-07 15:11:05 +0100

    Add Question model to admin, update URL patterns, and implement view functions with HTML template

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
+
 def index(request):
     return HttpResponse("Hello World!")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on questions" % question_id)
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
